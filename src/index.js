ae3ce7562314fa976011056182192a6d283d4b4b
NikoS <122801074+nnaikou@users.noreply.github.com>
2023-06-20 19:09:28 +0000

    Update 5 files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,48 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+const tbl = document.getElementById("populationTbl")
+const tblBody = document.getElementById("tblBody")
+let dataArr = []
+
+getdata()
+
+async function getdata() {
+  const url = "https://statfin.stat.fi/PxWeb/sq/4e244893-7761-4c4f-8e55-7a8d41d86eff"
+  const dataPromise = await fetch(url)
+  const dataJSON = await dataPromise.json()
+
+  const municipalities = Object.values(dataJSON.dataset.dimension.Alue.category.label)
+  let id = 0
+  municipalities.forEach((mun) => {
+    let obj = {city: mun, value: 0, theId: id}
+    dataArr.push(obj)
+    id++
+  })
+
+  const values = Object.values(dataJSON.dataset.value)
+  id = 0
+  values.forEach((val) => {
+    dataArr[id].value = val
+    id++
+  })
+
+  console.log(dataArr)
+
+  dataArr.forEach((obj) => {
+    let tr = document.createElement("tr")
+    let td1 = document.createElement("td")
+    let td2 = document.createElement("td")
+    td1.innerText = obj.city
+    td2.innerText = obj.value
+    tr.appendChild(td1)
+    tr.appendChild(td2)
+    tblBody.appendChild(tr)
+  })
+
+
+
+
+
+
+
+}
